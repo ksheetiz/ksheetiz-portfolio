7ea0eec3a008d31db162e4552966e8dd08d5a449
ksheetiz agrahari <ksheetiz43@gmail.com>
2026-02-22 16:58:09 +0530

    Chore : Updated the icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Ksheetiz Agrahari',
   tagline: 'Software Engineer · Full Stack Developer',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   future: {
     v4: true,
